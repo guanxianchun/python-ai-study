3ef5a138cc327967c5404cd955cea064e0637a58
guan.xianchun <guan.xianchun@eisoo.com>
2018-07-24 13:22:45 +0800

    提交K近邻算法及数据

--- com/spring/knn/knn.py
+++ com/spring/knn/knn.py
@@ -8,11 +8,44 @@ K邻近算法：采用测量不同特征值之间的距离方法进行分类
 '''
 import numpy as np
 import operator
+import matplotlib.pyplot as plt
 
+dating_label_map = {"didntLike":1, "smallDoses":2, "largeDoses":3}
+label_dating_map = {1:"didntLike", 2:"smallDoses", 3:"largeDoses"}
 def init_datas():
     group = np.array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
     labels = ['A', 'A', 'B', 'B']
     return group, labels
+
+def get_dating_data():
+    with open("dating_data.txt") as f:
+        all_lines = f.readlines()
+        line_size = len(all_lines)
+        return_matrix = np.zeros((line_size, 3))
+        index = 0
+        class_labels = []
+        for line in all_lines:
+            datas = line.strip().split("\t")
+            return_matrix[index, :] = datas[0:3]
+            class_labels.append(dating_label_map[datas[-1]])
+            index += 1
+        return return_matrix, class_labels
+
+def show_picture(x_data, y_data, labels):
+    fig, ax = plt.subplots()
+    ax.scatter(x_data, y_data, 15.0 * np.array(labels), 15.0 * np.array(labels))
+    plt.show()
+    
+    
+def autoNorm(data):
+    min_value = data.min(0)
+    max_value = data.max(0)
+    rangs = max_value - min_value
+    print min_value, max_value
+    m = data.shape[0]
+    normal_data = data - np.tile(min_value, (m, 1))
+    normal_data = normal_data / np.tile(rangs, (m, 1))
+    return normal_data, rangs, min_value
 
 def classify_0(in_x, train_datas, labels, k):
     """
@@ -36,8 +69,18 @@ def classify_0(in_x, train_datas, labels, k):
     # 对分类值进行降序排序
     sorted_class_count = sorted(class_count.iteritems(), key=operator.itemgetter(1), reverse=True)
     # 取最大值的分类标签
-    return sorted_class_count[0][0]
+    return label_dating_map[sorted_class_count[0][0]]
 
 if __name__ == '__main__':
-    train_data, labels = init_datas()
-    print classify_0([0.2, 0.2], train_data , labels, 3)
+    train_data, labels = get_dating_data()  # init_datas()
+    raw_data = [500, 8.9, 1.5]
+    show_picture(train_data[:, 0], train_data[:, 1], labels)
+#     show_picture(train_data[:, 0], train_data[:, 2], labels)
+#     show_picture(train_data[:, 1], train_data[:, 2], labels)
+    print 'classify: ', classify_0(raw_data, train_data, labels, 500)
+    # 对原始数据进行归一化处理
+    new_data, rangs, min_value = autoNorm(train_data)
+    show_picture(new_data[:, 0], new_data[:, 1], labels)
+    # 对输入数据进行归一化处理
+    normal_data = (np.array(raw_data) - min_value) / rangs
+    print 'classify: ', classify_0(normal_data, new_data, labels, 500)
